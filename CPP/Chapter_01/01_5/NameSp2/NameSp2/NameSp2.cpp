cac59fe7f1069bb12fc605803550c04942f365ea
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2020-12-27 03:50:02 +0900

    CPP언어 챕터1 업데이트

--- CPP/Chapter_01/01_5/NameSp2/NameSp2/NameSp2.cpp
+++ CPP/Chapter_01/01_5/NameSp2/NameSp2/NameSp2.cpp
@@ -1,28 +1,9 @@
-#include<iostream>
+#include "Nameheader.h"
 
-namespace BestComImpl
-{
-	void SimpleFunc(void);
-}
-
-namespace ProgComImpl
-{
-	void SimpleFunc(void);
-}
 
 int main(void)
 {
 	BestComImpl::SimpleFunc();
 	ProgComImpl::SimpleFunc();
 	return 0;
-}
-
-void BestComImpl::SimpleFunc(void)
-{
-	std::cout << "BestCom이 정의한 함수" << std::endl;
-}
-
-void ProgComImpl::SimpleFunc(void)
-{
-	std::cout << "ProgCom이 정의한 함수" << std::endl;
 }
---
+++ CPP/Chapter_01/01_5/NameSp2/NameSp2/Nameheader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<iostream>
+
+
+namespace BestComImpl
+{
+	void SimpleFunc(void);
+}
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void);
+}
