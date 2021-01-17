477d107fdddd69fa4af7ff51e88d4a40c6194c97
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2021-01-17 17:19:54 +0900

    조금더 업데이트

--- C/Homework/01/Return_02/Return_02/Return_02.c
+++ C/Homework/01/Return_02/Return_02/Return_02.c
@@ -1,13 +1,16 @@
 #include"header_file.h"
 
-int back_to_main(int num1) {
+char* back_to_main(int num1) {
 	int result = 0;
-	int arr[2] = {0, };
-
+	int arr[20] = {0, };
+	char fail[5] = "fail";
+	int i = 0;
 
 	if (num1<=10) {
-		arr[0] = 0;
-		arr[1] = "fail";
+		for (i = 0; i < 4; i++) {
+			arr[i] = fail[i];
+		}
+		arr[5] = 0;
 		return arr;
 	}
 	else {
@@ -20,11 +23,19 @@ int back_to_main(int num1) {
 
 void main() {
 	int num1 = 0;
-	int arr[2] = { 0, };
+	char * fail;
 	printf("정수를 입력 하시오.");
 	scanf_s("%d",&num1);
 
-	printf("%d", back_to_main(num1));
+	fail = back_to_main(num1);
+	//char fail[5] = "fail";
+	//int len = strlen(fail);
 
+	if (num1<10) {
+		for (int i = 0; i < 4; i++) {
+			printf("%c", fail[i]);
+		}
+		printf("%d", fail[5]);
+	}
 	return 0;
 }
