01defd3e6b12e8df551bf55fd67463e1001db0f2
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2021-01-19 00:02:23 +0900

    구조체 이용한 답변

--- C/Homework/01/Return_02/Return_02/Return_02.c
+++ C/Homework/01/Return_02/Return_02/Return_02.c
@@ -1,56 +1,33 @@
 #include"header_file.h"
 
-int* back_to_main(int num1) {
-	int* back_fail = malloc(sizeof(int)*10);
-	int* back_success = malloc(sizeof(int)*10);
-	char fail[5] = "fail", success[8] = "success";
-	int result = num1 * 10;
-	int i = 0;
+typedef struct data {
+	int num;
+	char* str;
+}Data;
 
+void ShowData(Data data) {
+	printf("%d \n", data.num);
+	printf("%s \n", data.str);
+}
+
+Data back_to_main(int num1) {
 	if (num1<=10) {
-		for (i = 0; i < 4; i++) {
-			back_fail[i] = fail[i];
-		}
-		back_fail[5] = 0;
-		return back_fail;
+		Data fail = { 0, "fail" };
+		return fail;
 	}
 	else {
-		for (i = 0; i < 7;i++) {
-			back_success[i] = success[i];
-		}
-		back_success[8] = result;
-		return back_success;
+		Data success = { num1 * 10, "success" };
+		return success;
 	}
-
 }
 
 void main() {
-	int num1 = 0, i=0;
-	int* fail = {0,};
-	int* success = {0, };
+	int num1 = 0;
 	printf("정수를 입력 하시오.");
 	scanf_s("%d",&num1);
+	Data data = back_to_main(num1);
 
-	if (num1 <= 10) {
-		fail = back_to_main(num1);
-	}
-	else {
-		success = back_to_main(num1);
-	}
-	//char fail[5] = "fail";
-	//int len = strlen(fail);
+	ShowData(data);
 
-	if (num1<=10) {
-		for (i = 0; i < 4; i++) {
-			printf("%c", fail[i]);
-		}
-		printf("%d", fail[5]);
-	}
-	else {
-		for (i = 0; i < 7;i++) {
-			printf("%c", success[i]);
-		}
-		printf("%d", success[8]);
-	}
 	return 0;
 }
