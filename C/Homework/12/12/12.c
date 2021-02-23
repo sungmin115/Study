3b54a06f5e23e94e2d46d59bbab91bf636e3ba51
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2021-02-24 08:20:10 +0900

    12 업데이트

---
+++ C/Homework/12/12/12.c
@@ -0,0 +1,11 @@
+#include"12.h"
+
+void main() {
+	int num1 = 0, num2 =0;
+	
+	main_display();
+	scanf("%d", &num1);
+	count(num1, num2);
+
+	return 0;
+}
---
+++ C/Homework/12/12/12.h
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#pragma warning(disable: 4996)
+
+void main_display() {
+	printf("자연수 N을 입력받는다 \n");
+	printf("N이 홀수인 경우에는 1부터 N까지의 홀수를 \n");
+	printf("N이 짝수인 경우에는 2부터 N까지의 짝수를 출력하는 프로그램\n");
+	printf("자연수 N을 입력 하시오.");
+}
+
+void count(int num1 ,int num2) {
+	
+	if (num1 %2 == 1) {
+		if (num1>0) {
+			num2 = num2 + 1;
+			printf("%d ", num2);
+			num1 = num1 - 2;
+			num2 = num2 + 1;
+			count(num1, num2);
+		}
+	}
+	else if(num1 %2 == 0) {
+		if (num1 > 0) {
+			num2 = num2 + 2;
+			printf("%d ", num2);
+			num1 = num1 - 2;
+			
+			count(num1, num2);
+		}
+	}
+}
