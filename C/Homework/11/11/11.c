beb75f3dba6e0cab24885daee64b21b1b062ee1e
SUNGMIN-COMPUTE\WaveM <zlbearlz@naver.com>
2021-02-24 06:55:14 +0900

    11번 문제 업데이트

---
+++ C/Homework/11/11/11.c
@@ -0,0 +1,12 @@
+#include"11.h"
+
+
+void main() {
+	int num1 = 0;
+
+	main_display();
+	scanf("%d", &num1);
+	count(num1);
+
+	return 0;
+}
---
+++ C/Homework/practice/practice/practice.cpp
@@ -0,0 +1,9 @@
+#include "practice.h"
+
+int main(void) {
+	MyFunc();
+	MyFunc('A');
+	MyFunc(12, 13);
+
+	return 0;
+}
